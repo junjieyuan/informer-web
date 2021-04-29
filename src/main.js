0bb27c011bad6d265d990f355b3500a2c61e4c0d
Junjie Yuan <yuan@junjie.pro>
2021-04-29 12:52:10 +0800

    remove # in url
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

--- src/main.js
+++ src/main.js
@@ -21,7 +21,8 @@ const routes = [
     {path: '/generate-password', component: GeneratePassword}
 ]
 const router = new VueRouter({
-        routes
+        mode: 'history',
+        routes: routes
     }
 )
 
