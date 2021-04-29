407833ef32a458da49001209f69639283548536f
Junjie Yuan <yuan@junjie.pro>
2021-04-29 09:35:04 +0800

    refactor logout
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

---
+++ src/components/Logout.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-btn @click="logout">注销</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Logout",
+  methods: {
+    logout: function () {
+      axios.post('/api/logout').then(function (response) {
+        if (response.status === 200) {
+          window.location.href = "/index.html"
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import HomepageContent from "@/components/HomepageContent";
 import Login from '@/components/Login'
+import Logout from '@/components/Logout'
 
 const routes = [
     {path: '/', component: HomepageContent},
     {path: '/login', component: Login},
+    {path: '/logout', component: Logout},
 ]
 const router = new VueRouter({
         routes
