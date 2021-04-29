161df9707c9f506b8aa7531aa8ca4b51dd78a819
Junjie Yuan <yuan@junjie.pro>
2021-04-29 09:30:19 +0800

    refactor login
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-text-field v-model="username" label="Username" required></v-text-field>
+      <v-text-field v-model="password" label="Password" required type="password"></v-text-field>
+      <v-btn @click="login">登录</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: "Login",
+  data: () => ({
+    username: '',
+    password: '',
+    loginField: ['username', 'password']
+  }),
+  methods: {
+    login: function () {
+      const json_login_info = JSON.stringify(this.$data, this.loginField);
+
+      axios.interceptors.response.use(
+          response => {
+            return response
+          },
+          error => {
+            if (error.response.status === 401) {
+              alert('用户名或密码错误，请重试')
+            }
+            return error
+          }
+      )
+
+      axios.post('/api/login', json_login_info).then(function (response) {
+        if (response.status === 200) {
+          window.location.href = "/libraries.html"
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import HomepageContent from "@/components/HomepageContent";
+import Login from '@/components/Login'
 
 const routes = [
     {path: '/', component: HomepageContent},
+    {path: '/login', component: Login},
 ]
 const router = new VueRouter({
         routes
