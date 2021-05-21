b4a7741b131d1ff8a9f26a31aaf4efdc6b42f266
Junjie Yuan <yuan@junjie.pro>
2021-05-21 11:29:08 +0800

    adjust homepage
    Signed-off-by: Junjie Yuan <yuan@junjie.pro>

--- src/components/HomepageContent.vue
+++ src/components/HomepageContent.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <p>
-      <a href="https://junjie.pro/informer">Informer</a>是一个不先进的密码管理器。
+      <a href="https://junjie.pro/informer">Informer</a>是一个密码管理器。
     </p>
     <p>
       如要开始使用，请先<router-link to="/login">登录</router-link>。
